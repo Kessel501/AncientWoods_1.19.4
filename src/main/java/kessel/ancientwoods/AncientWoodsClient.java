06f5876190b91c03a35a0ce4b45d4391ae593ccc
Korvou <Kevinpaz6665@gmail.com>
2023-04-19 20:38:05 -0500

    Initial commit

---
+++ src/main/java/kessel/ancientwoods/AncientWoodsClient.java
@@ -0,0 +1,11 @@
+package kessel.ancientwoods;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AncientWoodsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+
+    }
+}
